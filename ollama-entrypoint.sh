acebc5752434f3f526144ca58c2a5befdfd3fa7c
mpc <matthewcoole@gmail.com>
2025-01-20 15:46:05 +0000

    exposes data import pipeline to api

--- ollama-entrypoint.sh
+++ ollama-entrypoint.sh
@@ -5,4 +5,7 @@ sleep 5
 echo "Pulling model[$OLLAMA_MODEL]..."
 ollama pull $OLLAMA_MODEL
 echo "Finished pulling model"
+echo "Pulling model[$OLLAMA_EMBEDDING_MODEL]..."
+ollama pull $OLLAMA_EMBEDDING_MODEL
+echo "Finished pulling model"
 wait $pid
--- src/serka/eidc_fetcher.py
+++ src/serka/eidc_fetcher.py
@@ -6,6 +6,12 @@ from haystack_integrations.components.embedders.ollama import OllamaDocumentEmbe
 from haystack_integrations.document_stores.chroma import ChromaDocumentStore
 from haystack.components.writers import DocumentWriter
 from typing import List, Dict, Any, Set
+import logging
+
+logging.basicConfig(
+	level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 
 class EIDCPipeline:
@@ -13,7 +19,12 @@ class EIDCPipeline:
 		self,
 		content_field: str = "description",
 		meta_fields: Set[str] = {"identifier", "title"},
+		ollama_host: str = "ollama-container",
+		ollama_port: str = "11434",
 		embedding_model: str = "nomic-embed-text",
+		chroma_collection: str = "eidc",
+		chroma_host: str = "chromadb-container",
+		chroma_port: str = "8000",
 	) -> None:
 		self.fetcher = LinkContentFetcher()
 		self.converter = JSONConverter(
@@ -24,8 +35,14 @@ class EIDCPipeline:
 		self.splitter = DocumentSplitter(
 			split_by="word", split_length=150, split_overlap=50
 		)
-		self.embedder = OllamaDocumentEmbedder(model=embedding_model)
-		self.doc_store = ChromaDocumentStore(host="localhost", port="8001")
+		logger.info(f"Connecting to Ollama at {ollama_host}:{ollama_port}")
+		self.embedder = OllamaDocumentEmbedder(
+			url=f"http://{ollama_host}:{ollama_port}", model=embedding_model
+		)
+		logger.info(f"Connecting to Chroma at {chroma_host}:{chroma_port}")
+		self.doc_store = ChromaDocumentStore(
+			host=chroma_host, port=chroma_port, collection_name=chroma_collection
+		)
 		self.writer = DocumentWriter(document_store=self.doc_store)
 		self.pipeline = self._create_pipeline()
 
@@ -45,6 +62,7 @@ class EIDCPipeline:
 		return pipeline
 
 	def process(self, urls: List[str]) -> Dict[str, Any]:
+		logger.info(f"Processing URLS [{urls}]")
 		return self.pipeline.run(data={"fetcher": {"urls": urls}})
 
 
