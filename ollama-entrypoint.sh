2659c8897ea05f44ebf791c4f0db6b0c03be8dfe
mpc <matthewcoole@gmail.com>
2025-01-22 14:04:18 +0000

    Switched model pull shell script to make use of yaml config

--- ollama-entrypoint.sh
+++ ollama-entrypoint.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+# Start the ollama server
 /bin/ollama serve &
 pid=$!
 sleep 5
-echo "Pulling model[$OLLAMA_MODEL]..."
-ollama pull $OLLAMA_MODEL
-echo "Finished pulling model"
-echo "Pulling model[$OLLAMA_EMBEDDING_MODEL]..."
-ollama pull $OLLAMA_EMBEDDING_MODEL
-echo "Finished pulling model"
+
+# Install the yq utility for reading the yaml configuration
+apt update
+apt install wget -y
+wget https://github.com/mikefarah/yq/releases/download/v4.2.0/yq_linux_amd64 -O /usr/bin/yq
+chmod +x /usr/bin/yq
+
+# Pull the appropriate models listed in config.yaml
+readarray -t models < <(
+    yq eval '.ollama.embedding_models[]' config.yaml
+    yq eval '.ollama.rag_models[]' config.yaml
+)
+for model in "${models[@]}"; do
+    echo "Pulling model[$model]..."
+    ollama pull $model
+done
+echo "Finished pulling model(s)"
+
 wait $pid
