4bbbb672cbef3a3affd21ad1f6cd988d823b5b14
mpc <matthewcoole@gmail.com>
2025-02-21 11:50:31 +0000

    Created pydantic model for configuration

---
+++ src/serka/config.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from typing import List, Dict, Set
+
+
+class ChromaConfig(BaseModel):
+	host: str
+	port: int
+
+
+class OllamaConfig(BaseModel):
+	host: str
+	port: int
+
+
+class CollectionConfig(BaseModel):
+	source_name: str
+	description: str
+	organisation: str
+	url: str
+
+
+class Config(BaseModel):
+	chroma: ChromaConfig
+	ollama: OllamaConfig
+	embedding_models: List[str]
+	rag_models: List[str]
+	collections: Dict[str, CollectionConfig]
+	default_collection: str
+	rag_enabled: bool
+	unified_metadata: Set[str]
