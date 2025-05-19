9d5c9d4b03f4d29a4fa54eadb4c63a2d4c5c2163
mpc <matthewcoole@gmail.com>
2025-05-19 11:39:49 +0100

    initial legilo fetcher for graph pipeline

--- src/serka/__init__.py
+++ src/serka/__init__.py
@@ -1,17 +1,6 @@
 import logging
-import base64
-import os
-from dotenv import load_dotenv
-from haystack.components.fetchers.link_content import REQUEST_HEADERS
 
 # Set basic logging configuration
 logging.basicConfig(
 	level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
-
-# Load .env and setup default credentials for using the Legilo service.
-load_dotenv()
-user = os.environ.get("user")
-password = os.environ.get("password")
-credentials = base64.b64encode(f"{user}:{password}".encode("utf-8")).decode("utf-8")
-REQUEST_HEADERS["Authorization"] = f"Basic {credentials}"
--- src/serka/fetchers.py
+++ src/serka/fetchers.py
@@ -30,3 +30,40 @@ class EIDCFetcher:
 		)
 		eidc_data = res.json()
 		return {"records": eidc_data["results"]}
+
+
+@component
+class LegiloFetcher:
+	"""
+	Haystack fetcher component for retrieving supporting documentation from the Legilo API.
+	Args:
+		url (str): The URL of the Legilo API endpoint.
+	"""
+
+	def __init__(
+		self,
+		legilo_url: str = "https://legilo.eds-infra.ceh.ac.uk/{id}/documents",
+		legilo_username: str = None,
+		legilo_password: str = None,
+	):
+		self.legilo_url = legilo_url
+		self.legilo_username = legilo_username
+		self.legilo_password = legilo_password
+
+	@component.output_types(records=List[Dict[Any, Any]])
+	def run(self, dataset_ids: List[str]) -> List[Dict[Any, Any]]:
+		for id in dataset_ids:
+			url = self.legilo_url.format(id=id)
+			print(url)
+			res: Response = requests.get(
+				url,
+				auth=(self.legilo_username, self.legilo_password),
+			)
+			res.status_code
+			if res.status_code != 200:
+				print(f"Error: {res.status_code}")
+				return {"records": []}
+			if res.status_code == 200:
+				print(f"Success: {res.status_code}")
+				print(res.content)
+		return {"records": "results"}
